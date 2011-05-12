d1df6ee186319f7570e8e4279c44a101e5244ba7
root <root@ubuntu.localdomain>
2011-05-12 17:22:42 -0400

    first commit

---
+++ init_.php
@@ -0,0 +1,14 @@
+<?php
+Route::set('store', 'store/', array('provider' => '.+'))
+	->defaults(array(
+		'controller' => 'user',
+		'action'     => 'provider',
+		'provider'       => NULL,
+	));
+
+Route::set('user/provider_return', 'user/provider_return(/<provider>)', array('provider' => '.+'))
+	->defaults(array(
+		'controller' => 'user',
+		'action'     => 'provider_return',
+		'provider'       => NULL,
+	));
