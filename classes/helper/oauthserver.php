dfad5c8e9a34af811c9d8dc390426f4ff3b83209
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-05-13 10:24:21 -0700

    added some information related to the module

--- classes/helper/oauthserver.php
+++ classes/helper/oauthserver.php
@@ -2,7 +2,18 @@
 
 class Helper_Oauthserver {
 
+/**
+ * OAuthServer Helper class.
+ *
+ * @author
+ * - Jean-Nicolas Boulay Desjardins (http://jean-nicolas.name/)
+ * @package default
+ * @version 0.1 Alpha
+ */
+
 	public static function new_consumer_key() {
+
+
 
 	}
 
