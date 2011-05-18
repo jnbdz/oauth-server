7dc6c085281064ce6f3a13c8d293c3f2d81ecaf4
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-05-17 21:01:00 -0400

    added view

---
+++ views/oauth_server_add_app.php
@@ -0,0 +1,15 @@
+<form action="/" method="post">
+	<fieldset>
+		<legend>Register an Applicaton</legend>
+		<ol>
+			<li><label for="application_name">Application name: </label><input type="text" name="application_name" id="application_name"/></li>
+			<li><label for="description">Description: </label><textarea name="description" id="description"></textarea></li>
+			<li><label for="application_website">Application Website: </label><input type="text" name="application_website" id="application_website"/></li>
+			<li><label for="organization">Organization: </label><input type="text" name="organization" id="organization"/></li>
+			<li><label for="application_type">Application Type: </label>...</li>
+			<li><label for="callback_url">Callback URL: </label><input type="text" name="callback_url" id="callback_url"/></li>
+			<li><label for="default_access_type">Default Access type: </label>...</li>
+			<li><label for="application_icon">Application Icon: </label>...</li>
+		</ol>
+	</fieldset>
+</form>
